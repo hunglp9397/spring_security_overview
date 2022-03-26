2b094e1a91f48ea909db72ad6d95f54f3be4b74e
hunglp9397 <lephihung0997@gmail.com>
2022-03-26 21:24:35 +0700

    add user role and permission

--- src/main/java/com/hunglp/spring_security_overview/security/ApplicationSecurityConfig.java
+++ src/main/java/com/hunglp/spring_security_overview/security/ApplicationSecurityConfig.java
@@ -1,19 +1,50 @@
 package com.hunglp.spring_security_overview.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public  ApplicationSecurityConfig(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*")
-                .permitAll()
+                .antMatchers("/","index","/css/*","/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
                 .anyRequest()
                 .authenticated().and().httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails hunglp = User.builder()
+                .username("hunglp")
+                .password(passwordEncoder.encode("hunglp9397"))
+                .roles(ApplicationUserRole.STUDENT.name()).build();
+
+        User.builder()
+                .username("huyenbear")
+                .password(passwordEncoder.encode("huyenbear123"))
+                .roles(ApplicationUserRole.ADMIN.name()).build();
+
+        return new InMemoryUserDetailsManager(hunglp);
     }
 }
---
+++ src/main/java/com/hunglp/spring_security_overview/security/ApplicationUserRole.java
@@ -0,0 +1,19 @@
+package com.hunglp.spring_security_overview.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.hunglp.spring_security_overview.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+
+    }
+}
